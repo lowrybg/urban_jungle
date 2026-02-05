4d0fe73359a76cf83a0b74788f8ab2481acaf9e5
lowry <lowrybg@users.noreply.github.com>
2026-02-05 22:31:32 +0200

    add comment M2M
     one tip -> many plants

--- care/models.py
+++ care/models.py
@@ -7,6 +7,7 @@ class CareTip(models.Model):
     title = models.CharField(max_length=150)
     instructions = models.TextField()
 
+    #One Tip -> Many Plants
     related_plants = models.ManyToManyField(
     Plant,
     related_name='care_tips',
