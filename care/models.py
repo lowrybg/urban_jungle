5b3ebe5afdefe54ebb33d45d1c30dddd10518166
lowry <lowrybg@users.noreply.github.com>
2026-02-05 22:27:25 +0200

    add database models
    created Room, Plant, and CareTip models with FK and M2M relationships

--- care/models.py
+++ care/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from plants.models import Plant
+
+
+class CareTip(models.Model):
+    title = models.CharField(max_length=150)
+    instructions = models.TextField()
+
+    related_plants = models.ManyToManyField(
+    Plant,
+    related_name='care_tips',
+        blank=True,
+
+    )
+
+    def __str__(self):
+        return self.title
--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    SUNLIGHT_CHOICES = [
+        ('LOW', 'Low Light'),
+        ('MED', 'Medium Light'),
+        ('HIGH', 'Direct Sunlight'),
+    ]
+    name = models.CharField(
+        max_length=100,
+    )
+    light_level = models.CharField(max_length=4, choices=SUNLIGHT_CHOICES)
+    def __str__(self):
+        return self.name
+
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100, blank=True)
+
+    height_cm = models. FloatField(help_text="Height in centimeters")
+    water_needs_ml = models. PositiveSmallIntegerField(help_text="Water per week in milliliters")
+
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='plants',
+
+    )
+
+    photo = models.ImageField(upload_to='plants/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
