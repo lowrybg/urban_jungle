fdeee8c4064e67923f24f12593b9e20d18db60a5
lowry <lowrybg@users.noreply.github.com>
2026-02-10 19:16:32 +0200

    implement update and delete views

--- plants/urls.py
+++ plants/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PlantListView, PlantCreateView
+from .views import PlantListView, PlantCreateView, PlantUpdateView, PlantDeleteView
 
 urlpatterns = [
     path('catalog/', PlantListView.as_view(), name='catalog'),
     path('create/', PlantCreateView.as_view(), name='create_plant'),
+    path('edit/<int:pk>/', PlantUpdateView.as_view(), name='edit_plant'),
+    path('delete/<int:pk>/', PlantDeleteView.as_view(), name='delete_plant'),
 ]
--- plants/views.py
+++ plants/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .forms import PlantForm
 from .models import Plant
@@ -13,4 +13,16 @@ class PlantCreateView(CreateView):
     model = Plant
     form_class = PlantForm
     template_name = 'plants/create_plant.html'
+    success_url = reverse_lazy('catalog')
+
+
+class PlantUpdateView(UpdateView):
+    model = Plant
+    form_class = PlantForm
+    template_name = 'plants/edit_plant.html'
+    success_url = reverse_lazy('catalog')
+
+class PlantDeleteView(DeleteView):
+    model = Plant
+    template_name = 'plants/delete_plant.html'
     success_url = reverse_lazy('catalog')
