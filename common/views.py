a9d097eb36a2795343abe7f6f6a148475907d823
lowry <lowrybg@users.noreply.github.com>
2026-02-24 01:47:32 +0200

    update home and add dynamic dashboard stats
    updated the home view to count the database records so the homepage now shows the total number of plants and rooms

--- common/views.py
+++ common/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from plants.models import Plant, Room
 class HomeView(TemplateView):
     template_name = 'common/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_plants'] = Plant.objects.count()
+        context['total_rooms'] = Room.objects.count()
+        return context
 
 def custom_404(request, exception):
     return render(request, 'common/404.html', status=404)
